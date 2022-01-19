76f8857c93e14554097449d135a0fe3d2cd8428e
Ngoaduy <44079480+Ngoaduy@users.noreply.github.com>
2022-01-19 10:44:30 -0500

    first commit

---
+++ imports/ui/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let counter = 0;
+  const addToCounter = () => {
+    counter += 1;
+  }
+</script>
+
+
+<div class="container">
+  <h1>Current Inventory</h1>
+  
+
+  <button on:click={addToCounter}>Click Me</button>
+  <p>You've pressed the button {counter} times.</p>
+
+  <form id= "imageForm">
+    <input id="imageInput" type="file" accept="image/*">
+    <button type="submit">Upload</button>
+  </form>
+
+  <ul>
+    
+  </ul>
+</div>
---
+++ imports/ui/Items.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { meteor } from 'meteor/meteor'; 
+    import ItemsCollection from '../api/ItemsCollection'
+    
+    
+
+</script>
+
